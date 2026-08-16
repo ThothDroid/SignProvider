872991f45a2496df87b5f3195024b4a36359e9ae
ThothDroid <website-tutorials@gmx.de>
2026-08-16 18:08:26 +0200

    increment version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,15 +5,15 @@ plugins {
 android {
     namespace = "com.blueapps.signproviderexampleapp"
     compileSdk {
-        version = release(36)
+        version = release(37)
     }
 
     defaultConfig {
         applicationId = "com.blueapps.signproviderexampleapp"
         minSdk = 24
-        targetSdk = 36
-        versionCode = 102
-        versionName = "08.02.2026@1.0.2"
+        targetSdk = 37
+        versionCode = 110
+        versionName = "16.08.2026@1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
